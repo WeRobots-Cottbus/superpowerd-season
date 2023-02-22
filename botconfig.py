6ccc74927622ce4eb1cf20e5dcf4b995540eb6ac
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-02-22 14:17:28 +0100

    update error msg

--- botconfig.py
+++ botconfig.py
@@ -21,53 +21,55 @@ def wait_exit(ms:int=2_000):
     wait(ms)
     sys.exit()
 
+print_error = lambda port, name: f"[ERR] Port {port}: '{name}'"
+
 # Init Motors
 try:
     MotorFront = Motor(Port.A)
 except:
-    print_message("[ERROR] Port A: 'Motor Front'")
+    print_error("A", "Motor Front")
     wait_exit()
 try:
     MotorLeft = Motor(Port.B)
 except:
-    print_message("[ERROR] Port B: 'Motor Left'")
+    print_error("B", "Motor Left")
     wait_exit()
 try:
     MotorRight = Motor(Port.C)
 except:
-    print_message("[ERROR] Port C: 'Motor Right'")
+    print_error("C", "Motor Right")
     wait_exit()
 try:
     MotorTop = Motor(Port.D)
 except:
-    print_message("[ERROR] Port D: 'Motor Top'")
+    print_error("D", "Motor Top")
     wait_exit()
 
 # Init Sensors
 try:
-    Gyro= GyroSensor(Port.S1)
+    Gyro = GyroSensor(Port.S1)
 except:
-    print_message("[ERROR] Port 1: 'Gyro'")
+    print_error("1", "Gyro")
     wait_exit()
 try:
     ColorRight = ColorSensor(Port.S2)
 except:
-    print_message("[ERROR] Port 2: 'Color Right'")
+    print_error("2", "Color Right")
     wait_exit()
 try:
     ColorLeft  = ColorSensor(Port.S3)
 except:
-    print_message("[ERROR] Port 3: 'Color Left'")
+    print_error("3", "Color Left")
     wait_exit()
 try:
     ColorDetect = ColorSensor(Port.S4)
 except:
-    print_message("[ERROR] Port 4: 'Color Detect'")
+    print_error("4", "Color Detect")
     wait_exit()
 
 # Init Base
 try:
     Base = DriveBase(MotorLeft, MotorRight, 57, 154)
 except:
-    print_message("[ERROR] Drivebase")
+    print_message("[ERR] Drivebase")
     wait_exit()
