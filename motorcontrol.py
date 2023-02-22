a65913b369f1208a8e4c3e7e7448bbcabee42c3f
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-02-22 17:17:10 +0100

    typos

--- motorcontrol.py
+++ motorcontrol.py
@@ -10,24 +10,25 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 from botconfig import *
 from toolkit import *
 
-mtr_lst = [(MotorTop, MotorFront), (MotorLeft, MotorRight)]
-mtr_len = len(mtr_lst)
+MTR_LST = [(MotorTop, MotorFront), (MotorLeft, MotorRight)]
 
 def loop():
+    global MTR_LST
     speed = 300
     mtr_sel = 0
+
     while True:
         # select motors
         if Button.CENTER in Brick.buttons.pressed(): mtr_sel = not mtr_sel
         
         # run or stop motors
-        elif Button.UP    in Brick.buttons.pressed(): mtr_lst[mtr_sel][0].run(+speed)
-        elif Button.DOWN  in Brick.buttons.pressed(): mtr_lst[mtr_sel][0].run(-speed)
-        elif Button.LEFT  in Brick.buttons.pressed(): mtr_lst[mtr_sel][1].run(-speed)
-        elif Button.RIGHT in Brick.buttons.pressed(): mtr_lst[mtr_sel][1].run(+speed)
+        elif Button.UP    in Brick.buttons.pressed(): MTR_LST[mtr_sel][0].run(+speed)
+        elif Button.DOWN  in Brick.buttons.pressed(): MTR_LST[mtr_sel][0].run(-speed)
+        elif Button.LEFT  in Brick.buttons.pressed(): MTR_LST[mtr_sel][1].run(-speed)
+        elif Button.RIGHT in Brick.buttons.pressed(): MTR_LST[mtr_sel][1].run(+speed)
         else:
-            mtr_lst[mtr_sel][0].stop()
-            mtr_lst[mtr_sel][1].stop()            
+            MTR_LST[mtr_sel][0].stop()
+            MTR_LST[mtr_sel][1].stop()
 
         # update display if center button is pressed
         if Button.CENTER in Brick.buttons.pressed():
