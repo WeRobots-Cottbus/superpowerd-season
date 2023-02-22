f17f8c9787c6bb1ffa3b7cef1a46113bf1709b64
FabianBartl <fabian@informatic-freak.de>
2023-02-22 23:43:30 +0100

    minor

--- motorcontrol.py
+++ motorcontrol.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, GyroSensor
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.parameters import Button
+from pybricks.tools import wait
 
 from botconfig import *
 from toolkit import *
