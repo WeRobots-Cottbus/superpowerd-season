a7527f829e88d19b6abb913a401f7999cea4ef82
FabianBartl <fabian@informatic-freak.de>
2023-02-23 01:16:02 +0100

    comment

--- toolkit.py
+++ toolkit.py
@@ -24,7 +24,7 @@ def DisplayText(text:str, coord:tuple[int,int]=(0,0), clear:bool=True, **kwargs)
     if clear: Brick.screen.clear()
     Brick.screen.draw_text(coord[0], coord[1], text, **kwargs)
 
-"""       -        0        +
+"""      -77       0       +77
 pivot:    |--------x--------|   """
 def TurnOnPivot(pivot:float, relative_angle:float, speed:float, stop:Stop=Stop.HOLD) -> None:
     if relative_angle == 0: return
@@ -64,7 +64,7 @@ def GyroDrive(distance:int, speed:float=200, gyro_start:int=0, factor:int=3, bra
 #drehung auf einen gyrowert
 #nutzung in userem Program nicht als jede drehung einzel sonder ein 0 wert wird gesetzt un darauf wird
 #werden dann alle andere werte berchnet. So wird dreht sich der roboter bei 90 grad immer auf die gleichen 90 grad.
-def GyroTurn(angle:int,speed:int=20) -> None: 
+def GyroTurn(angle:int, speed:int=20) -> None: 
     #drehung nach rechts
     if angle >= 0:
         while Gyro.angle() <= angle:#drehung solange bis gyro richtigen wert gibt
